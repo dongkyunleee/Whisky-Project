7392a987a471593a1435167269606c25bcf1900f
dongkyunleee <coat000@naver.com>
2023-04-17 10:10:56 +0900

    밑바탕ㅅ

---
+++ src/main/java/com/example/WiskyProject/WiskyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.WiskyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiskyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiskyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WiskyProject/WiskyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WiskyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiskyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
